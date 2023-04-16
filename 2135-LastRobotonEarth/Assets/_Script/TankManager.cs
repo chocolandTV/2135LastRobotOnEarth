cd71a8483e182d64585c90aa4680a448399d81cb
Robert Moegenburg <r.moegenburg@t-online.de>
2023-04-16 10:52:28 +0200

    tankManager

--- 2135-LastRobotonEarth/Assets/_Script/TankManager.cs
+++ 2135-LastRobotonEarth/Assets/_Script/TankManager.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class TankManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private GameObject[] tankIcons;
+    [SerializeField] private TextMeshProUGUI tankTexts;
+    private GameObject lastTankIcon;
+    public static TankManager Instance;
+    private void Awake() {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    ///////////////  UI CHANGING //////////////////
+    public void OnChangeValue(int Value)
     {
-        
+        if(Value == 0)
+        {
+            lastTankIcon.SetActive(false);
+            tankIcons[0].SetActive(true);
+            lastTankIcon = tankIcons[0];
+
+            tankTexts.text = "" + Value + " %";
+        }else if ( Value < 20)
+        {
+            lastTankIcon.SetActive(false);
+            tankIcons[1].SetActive(true);
+            lastTankIcon = tankIcons[1];
+
+            tankTexts.text = "" + Value + " %";
+        }
+        else if ( Value < 40)
+        {
+            lastTankIcon.SetActive(false);
+            tankIcons[2].SetActive(true);
+            lastTankIcon = tankIcons[2];
+
+            tankTexts.text = "" + Value + " %";
+        }
+        else if ( Value < 60)
+        {
+            lastTankIcon.SetActive(false);
+            tankIcons[3].SetActive(true);
+            lastTankIcon = tankIcons[3];
+
+            tankTexts.text = "" + Value + " %";
+        }
+        else if ( Value < 80)
+        {
+            lastTankIcon.SetActive(false);
+            tankIcons[4].SetActive(true);
+            lastTankIcon = tankIcons[4];
+
+            tankTexts.text = "" + Value + " %";
+        }
+        else if ( Value >= 100)
+        {
+            lastTankIcon.SetActive(false);
+            tankIcons[5].SetActive(true);
+            lastTankIcon = tankIcons[5];
+
+            tankTexts.text = "100 %";
+        }
     }
 }
