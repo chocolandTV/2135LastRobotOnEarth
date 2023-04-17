deed4cec0df02bb31aa36e2e2acc2c70fae897d7
Robert Moegenburg <r.moegenburg@t-online.de>
2023-04-17 18:09:39 +0200

    TerraformerUupgradeUI

--- 2135-LastRobotonEarth/Assets/_Script/Upgrade/UpgradeTrack.cs
+++ 2135-LastRobotonEarth/Assets/_Script/Upgrade/UpgradeTrack.cs
@@ -2,20 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradeTrack : MonoBehaviour, IUpgradable
+public class UpgradeTrack : MonoBehaviour
 {
 
-   public void SetValue(float value){
-
-   }
-   public void SubtractCosts(int costs)
-   {
-
-   }
-   public void SetLevel(int level)
-   {
-
-   }
+   
     // Start is called before the first frame update
     void Start()
     {
