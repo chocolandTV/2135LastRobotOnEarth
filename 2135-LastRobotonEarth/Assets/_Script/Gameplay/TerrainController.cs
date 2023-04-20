61bcdf2ff52f1b5181d8900aa868a45755b4cb09
Robert Moegenburg <r.moegenburg@t-online.de>
2023-04-20 21:21:44 +0200

    TerrainExplosion

--- 2135-LastRobotonEarth/Assets/_Script/Gameplay/TerrainController.cs
+++ 2135-LastRobotonEarth/Assets/_Script/Gameplay/TerrainController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 using UnityEngine.TerrainTools;
 public class TerrainController : MonoBehaviour
 {
-    [SerializeField] private TerrainData alphaTest01;
+    [SerializeField] private TerrainData[] alphaMaps;
     private Terrain terrain;
+    [SerializeField] private ParticleSystem terrainParticleSystem;
+    ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
     // Start is called before the first frame update
     void Start()
     {
         terrain = GetComponent<Terrain>();
-        terrain.terrainData= alphaTest01;
+        terrain.terrainData= alphaMaps[0]; // CHANGE ON PLAYERPEFS
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeTerrainData(int value)
     {
-        
+        terrain.terrainData= alphaMaps[value];
+        terrainParticleSystem.Emit(emitParams,100 * (int)VariableManager.Instance.Game_Terraformer_mission);
     }
 }
