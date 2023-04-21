cbab0c49ae2fa7f12e971ce9efd4f49146f7911d
Robert Moegenburg <r.moegenburg@t-online.de>
2023-04-21 23:08:50 +0200

    moon

---
+++ 2135-LastRobotonEarth/Assets/_Script/Effect/Moon.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    
+    Vector3 rotation = new Vector3(0,10,0);
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = Vector3.Distance(transform.position, PlayerController.Instance.gameObject.transform.position);
+    }
+    // ROTATE 
+    private void Rotate()
+    {
+        transform.Rotate(rotation*Time.fixedDeltaTime);
+    }
+    // DISTANCE TO PLAYER 
+    private void KeepDistance()
+    {
+        transform.position = (transform.position - PlayerController.Instance.gameObject.transform.position).normalized * distance + PlayerController.Instance.gameObject.transform.position;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rotate();
+        KeepDistance();
+    }
+}
